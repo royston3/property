332ed7f6fd6f5ef4389229c347f58d67b901f106
Royston Raphael <royston.raphael@xero.com>
2017-06-27 09:48:15 +1200

    new project setup

---
+++ property/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace property
+{
+    [Activity(Label = "property", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
